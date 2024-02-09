4dfd75af860927a5d61e4c7dd5994f13c985a41f
it21307126 <it21307126@my.sliit.lk>
2024-02-10 02:26:51 +0530

    weather1

--- src/components/weather app/WeatherApp.jsx
+++ src/components/weather app/WeatherApp.jsx
@@ -8,17 +8,23 @@ import rain from "../assets/rain.png"
 import wind from "../assets/wind.png"
 import snow from "../assets/snow.png"
 import humidity from "../assets/humidity.png"
-
+import sky from "../assets/Timelapse clouds Full HD Free stock footage _ Relaxing morning sky.mp4"
 
 const WeatherApp = () => {
   return (
     <div className='container'>
+        <video autoPlay loop muted className="video-bg">
+        <source src={sky} type="video/mp4" />
+        {/* Add additional source tags for other video formats if needed */}
+        Your browser does not support the video tag.
+      </video>
       <div className='top-bar'>  
         <input type="text" className='cityInput' placeholder='Search' /> 
             <div className='search-icon'>
                 <img src={search} alt="" />
 
             </div>
+            
       </div>
       <div className='weather-image'>
         <img src={cloud} alt="" />
